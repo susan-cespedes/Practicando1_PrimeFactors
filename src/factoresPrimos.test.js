dcf9496cdc03b7266329478a5ca407ff4e5620bd
Susan Cespedes <susan.cespedes@ucb.edu.bo>
2023-09-24 20:15:03 -0400

    Deberia retornar una lista para [2]

--- src/factoresPrimos.test.js
+++ src/factoresPrimos.test.js
@@ -1,13 +1,22 @@
 describe("FACTORES PRMOS", () => {
-  it("deberia retornar una lista vacia para 1", () => {
+  it("Deberia retornar una lista vacia para 1", () => {
     const primosfac = new primos();
     expect(primosfac.factorprimo(1)).toEqual([]);
+  });
+  it("Deberia retornar una lista para [2]", () => {
+    const primosfac = new primos();
+    expect(primosfac.factorprimo(2)).toEqual([2]);
   });
 });
 
 
 class primos{
-  factorprimo(){
-    return [];
+  factorprimo(n){
+    let resulprimos =[];
+    if (n>1)
+    {
+      resulprimos.push (2);
+    }
+    return resulprimos;
   }
 }
